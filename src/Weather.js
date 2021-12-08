1f4030f3315d0d19e1daa877161010581ba7b33b
lydia merrix <lydiamerrix@gmail.com>
2021-12-08 14:02:47 -0500

    adjusted brackets

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,14 @@ export default function Weather(props) {
   ${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000}
+    />
+  );
 }
-return (
-  <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000} />
-);
