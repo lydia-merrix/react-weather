31be920b036efa4f4c3f19f09922674e89a565e6
lydia merrix <lydiamerrix@gmail.com>
2021-12-08 13:33:55 -0500

    working on spinner

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Hello World </h1>
-        <weather city="new york" />
+        <Weather city="new york" />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -15,3 +16,6 @@ export default function Weather(props) {
 
   axios.get(apiUrl).then(handleResponse);
 }
+return (
+  <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000} />
+);
